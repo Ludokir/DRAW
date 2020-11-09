43269df8d45c6c96b1b6797ba8365981c18e84e6
Ludokir <stepanmedinsky@yandex.ru>
2020-11-09 17:26:33 +1000

    -

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pygame
 import os
+import random
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-WIDTH_WIN, HEIGHT_WIN = 400, 400
+WIDTH_WIN, HEIGHT_WIN = 800, 800
 collide = False
+collide2 = False
 num = 0
 num1 = 0
+num2 = 0
+speed_x = random.randint(4, 10)
+speed_y = random.randint(4, 10)
 block = False
 collide1 = False
+collide2 = False
 g = False
+m = False
 
-rect_size = w, h = 70, 70
+rect_size = w, h = 300, 300
 rect_pos = ((WIDTH_WIN - w) // 2, (HEIGHT_WIN - h) // 2)
 
 pol_size = w, h = 50, 50
@@ -30,7 +37,7 @@ GREEN = (0, 255, 0)
 pygame.init()
 pygame.display.set_caption('DRAW and COLLIDE')
 screen = pygame.display.set_mode((WIDTH_WIN, HEIGHT_WIN))
-FPS = 60
+FPS = 120
 clock = pygame.time.Clock()
 
 surface = pygame.Surface(
@@ -39,8 +46,20 @@ pygame.draw.circle(
     surface, YELLOW, (circle_radius, circle_radius), circle_radius)
 rect1 = surface.get_rect()
 
+ball = pygame.image.load('img/ball.png')
+ball_rect = ball.get_rect(topleft=(0, 0))
+
 font = pygame.font.SysFont('Arial', 20, True, False)
 font1 = pygame.font.SysFont('Arial', 20, True, False)
+font2 = pygame.font.SysFont('Arial', 20, True, False)
+
+
+def mo(x, y):
+    if ball_rect.left < 0 or ball_rect.right > WIDTH_WIN:
+        x = -x
+    if ball_rect.top < 0 or ball_rect.bottom > HEIGHT_WIN:
+        y = -y
+
 
 run = True
 while run:
@@ -56,17 +75,23 @@ while run:
     screen.fill(BG)
 
     rect2 = pygame.draw.rect(
-        screen, RED if collide else BLUE, (rect_pos, rect_size))
-    rect3 = pygame.draw.polygon(screen, RED if collide1 else GREEN, [
-        [300, 300], [350, 300], [325, 250]])
+        screen, RED if collide or collide1 else BLUE, (rect_pos, rect_size))
+    rect3 = pygame.draw.polygon(screen, RED if collide2 else GREEN, [
+        [600, 700], [700, 700], [650, 600]])
 
     text = font.render(str(num), 1, BLACK)
-    text_render = text.get_rect(center=(300, 50))
+    text_render = text.get_rect(center=((WIDTH_WIN - 100), 50))
 
-    text1 = font1.render(str(num1), 1, BLACK)
+    text1 = font1.render(str(num1), 1, BLUE)
     text_render1 = text1.get_rect(center=(100, 50))
 
-    if rect1.colliderect(rect3):
+    text2 = font2.render(str(num2), 1, GREEN)
+    text_render2 = text2.get_rect(center=(400, 50))
+
+    ball_rect = ball_rect.move(speed_x, speed_y)
+    mo(speed_x, speed_y)
+
+    if ball_rect.colliderect(rect2):
         collide1 = True
         if not g:
             num1 += 1
@@ -84,8 +109,19 @@ while run:
         collide = False
         block = False
 
+    if rect1.colliderect(rect3):
+        collide2 = True
+        if not m:
+            num2 += 1
+        m = True
+    else:
+        collide2 = False
+        m = False
+
+    screen.blit(text2, text_render2)
     screen.blit(surface, rect1)
     screen.blit(text, text_render)
     screen.blit(text1, text_render1)
+    screen.blit(ball, ball_rect)
     pygame.display.update()
     clock.tick(FPS)
